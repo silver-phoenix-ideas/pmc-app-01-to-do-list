b72bf2ebb92d4117babeaf19a27e6a0bbb4b1113
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-05 20:46:05 -0500

    Implement basic CLI functionality for capturing and displaying to do items.

---
+++ main.py
@@ -0,0 +1,8 @@
+prompt = "Enter to do item: "
+user_input1 = input(prompt)
+user_input2 = input(prompt)
+user_input3 = input(prompt)
+
+todos = [user_input1, user_input2, user_input3]
+
+print(type(todos), todos)
