c943ebd3c6e7245fc259446e3de958a3b5a7eed3
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-07 19:25:36 -0500

    Implement functionality for adding to do items, displaying the to do list and exiting the program.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-prompt = "Enter to do item: "
+prompt = "Type add, show or exit: "
 todo_list = []
 
 while True:
-    user_input = input(prompt)
-    todo_list.append(user_input.title())
-    print(todo_list)
+    user_action = input(prompt).strip().casefold()
+
+    match user_action:
+        case "add":
+            todo_item = input("Enter to do item: ")
+            todo_list.append(todo_item.title())
+        case "show":
+            for todo_item in todo_list:
+                print("-", todo_item)
+        case "exit":
+            print("Bye!")
+            break
+        case _:
+            print("Unknown command.")
