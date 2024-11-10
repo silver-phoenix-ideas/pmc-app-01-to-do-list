df97558e13a1360a7b91d0e1322ff36c7610d9fb
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-10 16:58:47 -0500

    Implement functionality for updating the text file when editing and completing to do items. Refactor code to use with context manager.

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-prompt = "Type add, show, edit, complete or exit: "
 filename = "to_do_list.txt"
 
-file = open(filename)
-todo_list = file.read().split("\n")
-file.close()
+with open(filename) as file:
+    todo_list = file.read().split("\n")
 
 while True:
-    user_action = input(prompt).strip().casefold()
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip().casefold()
 
     match user_action:
         case "add":
-            todo_item = input("Enter to do item: ")
+            todo_item = input("Enter new item: ")
             todo_item = todo_item.strip().title()
             todo_list.append(todo_item)
 
-            file = open(filename, 'w')
-            file.write("\n".join(todo_list))
-            file.close()
+            with open(filename, 'w') as file:
+                file.write("\n".join(todo_list))
+
         case "show":
             for index, todo_item in enumerate(todo_list, start=1):
                 print(index, "-", todo_item)
+
         case "edit":
             index = int(input("Which item do you want to edit? ")) - 1
-            todo_item = input("Enter new to do item: ")
+            todo_item = input("Enter new text: ")
             todo_list[index] = todo_item.strip().title()
+
+            with open(filename, 'w') as file:
+                file.write("\n".join(todo_list))
+
         case "complete":
             index = int(input("Which item do you want to complete? ")) - 1
-            todo_list.pop(index)
+            todo_item = todo_list.pop(index)
+            print(f'Item "{todo_item}" was successfully removed.')
+
+            with open(filename, 'w') as file:
+                file.write("\n".join(todo_list))
+
         case "exit":
             print("Bye!")
             break
+
         case _:
             print("Unknown command.")
