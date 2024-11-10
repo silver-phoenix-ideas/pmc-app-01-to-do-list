316ec949a4a110bb31a703a8ba7a26e06ff5e869
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-10 18:55:49 -0500

    Refactor code by declaring variables for all localization texts at the top of the script.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+# Configuration
 filename = "to_do_list.txt"
 
+# Localization
+txt_action_prompt = "Type add, show, edit, complete or exit:"
+txt_action_error = "Unknown command."
+txt_add_prompt = "Enter new item:"
+txt_edit_prompt_index = "Which item do you want to edit?"
+txt_edit_prompt_value = "Enter new text: "
+txt_complete_prompt = "Which item do you want to complete?"
+txt_complete_success = 'Item "{0}" has been removed.'
+txt_exit = "Bye!"
+
+# Logic
 with open(filename) as file:
     todo_list = file.read().split("\n")
 
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = input(txt_action_prompt + " ")
     user_action = user_action.strip().casefold()
 
     match user_action:
         case "add":
-            todo_item = input("Enter new item: ")
+            todo_item = input(txt_add_prompt + " ")
             todo_item = todo_item.strip().title()
             todo_list.append(todo_item)
 
@@ -21,17 +33,17 @@ while True:
                 print(index, "-", todo_item)
 
         case "edit":
-            index = int(input("Which item do you want to edit? ")) - 1
-            todo_item = input("Enter new text: ")
+            index = int(input(txt_edit_prompt_index + " ")) - 1
+            todo_item = input(txt_edit_prompt_value + " ")
             todo_list[index] = todo_item.strip().title()
 
             with open(filename, 'w') as file:
                 file.write("\n".join(todo_list))
 
         case "complete":
-            index = int(input("Which item do you want to complete? ")) - 1
+            index = int(input(txt_complete_prompt + " ")) - 1
             todo_item = todo_list.pop(index)
-            print(f'Item "{todo_item}" was successfully removed.')
+            print(txt_complete_success.format(todo_item))
 
             with open(filename, 'w') as file:
                 file.write("\n".join(todo_list))
@@ -41,4 +53,4 @@ while True:
             break
 
         case _:
-            print("Unknown command.")
+            print(txt_action_error)
