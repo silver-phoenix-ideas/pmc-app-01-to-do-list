4c1f48b3bb0b38b4470d2b783d339d47ef1cc7df
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-16 20:37:15 -0500

    Add type hinting to all function definitions.

--- modules/functions.py
+++ modules/functions.py
@@ -1,20 +1,20 @@
 import configuration.config as config
 
 
-def get_todo_list(filename=config.FILENAME):
+def get_todo_list(filename: str = config.FILENAME):
     """ Reads the contents of a text file and returns a to-do list. """
     with open("files/" + filename) as file:
         todo_list = file.read().split("\n")
     return todo_list
 
 
-def save_todo_list(todo_list, filename=config.FILENAME):
+def save_todo_list(todo_list: list, filename: str = config.FILENAME):
     """ Saves the contents of a to-do list in a text file. """
     with open("files/" + filename, 'w') as file:
         file.write("\n".join(todo_list))
 
 
-def display_todo_list(todo_list):
+def display_todo_list(todo_list: list):
     """ Prints out to-do list items and their numbers in the console. """
     for index, todo_item in enumerate(todo_list, start=1):
         print(index, "-", todo_item)
