08ad700b37716f83964bf135e99d56679b6a96ba
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-20 21:53:29 -0500

    Update GUI layout and appearance. Substitute input field label with a tooltip.

--- gui.py
+++ gui.py
@@ -4,30 +4,51 @@ import FreeSimpleGUI as fsg
 import localization.texts as txt
 import modules.functions as functions
 
+# Settings
+fsg.theme("DarkGrey9")
+fsg.set_options(font=("Helvetica", 12))
+max_width = 50
+button_size = 10
+pad = (5, 15)
+popup_duration = 1
+
 # Widgets
-clock = fsg.Text(time.strftime(txt.datetime_format), key="clock")
-label = fsg.Text(txt.add_prompt)
-input_field = fsg.Input(key="todo_item")
-add_button = fsg.Button(txt.add_button, key="add")
+title = fsg.Text(txt.app_title, font=("Helvetica", 14, "bold"), pad=pad)
+clock = fsg.Text(
+    time.strftime(txt.datetime_format), key="clock", font=("Helvetica", 10)
+)
 todo_list_box = fsg.Listbox(
     values=functions.get_todo_list(), key="todo_list",
-    enable_events=True, size=(45, 10)
+    enable_events=True, no_scrollbar=True, size=(max_width, 10)
 )
-edit_button = fsg.Button(txt.edit_button, key="edit")
-complete_button = fsg.Button(txt.complete_button, key="complete")
-close_button = fsg.Button(txt.close_button, key="close")
+todo_item_field = fsg.Input(
+    key="todo_item", size=max_width, tooltip=txt.add_prompt
+)
+add_button = fsg.Button(
+    txt.add_button, key="add", size=button_size, pad=pad
+)
+edit_button = fsg.Button(
+    txt.edit_button, key="edit", size=button_size, pad=pad
+)
+complete_button = fsg.Button(
+    txt.complete_button, key="complete", size=button_size, pad=pad
+)
+close_button = fsg.Button(
+    txt.close_button, key="close", size=button_size, pad=pad
+)
 
 # Layout
 layout = [
-    [clock],
-    [label],
-    [input_field, add_button],
-    [todo_list_box, edit_button, complete_button],
-    [close_button]
+    [title, fsg.Push(), clock],
+    [todo_list_box],
+    [todo_item_field],
+    [add_button, edit_button, complete_button, fsg.Push(), close_button]
 ]
 
 # Window
-window = fsg.Window(txt.app_title, layout=layout, font=("Helvetica", 12))
+window = fsg.Window(
+    txt.app_title, layout=layout, no_titlebar=True, grab_anywhere=True
+)
 
 # Logic
 while True:
@@ -44,13 +65,19 @@ while True:
                 window["todo_item"].update(value="")
                 window["todo_list"].update(values=functions.get_todo_list())
             except ValueError as e:
-                fsg.popup_quick_message(txt.error_empty_string)
+                fsg.popup_quick_message(
+                    txt.error_empty_string,
+                    auto_close_duration=popup_duration
+                )
 
         case "edit":
             try:
                 old_item = data["todo_list"][0]
             except IndexError:
-                fsg.popup_quick_message(txt.error_no_selected_item)
+                fsg.popup_quick_message(
+                    txt.error_no_selected_item,
+                    auto_close_duration=popup_duration
+                )
                 continue
 
             new_item = data["todo_item"]
@@ -70,7 +97,10 @@ while True:
             try:
                 todo_item = data["todo_list"][0]
             except IndexError:
-                fsg.popup_quick_message(txt.error_no_selected_item)
+                fsg.popup_quick_message(
+                    txt.error_no_selected_item,
+                    auto_close_duration=popup_duration
+                )
                 continue
 
             todo_list = functions.get_todo_list()
