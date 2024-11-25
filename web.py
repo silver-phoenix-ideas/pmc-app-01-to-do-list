20402a723c6a9daa7de45a8d554de3493bbfe1a3
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-24 19:59:19 -0500

    Implement functionality for adding to-do items via the web interface.

--- web.py
+++ web.py
@@ -1,12 +1,37 @@
+# Imports
 import streamlit as st
 import localization.texts as txt
 import modules.functions as functions
 
-todo_list = functions.get_todo_list()
 
-st.title(txt.app_title)
+# Logic
+def add_todo_item():
+    todo_item = st.session_state["todo_item"]
 
-for todo_item in todo_list:
-    st.checkbox(todo_item)
+    try:
+        functions.add_todo_item(todo_item)
+    except ValueError as e:
+        st.session_state["error"] = txt.error_empty_string
 
-st.text_input("", placeholder=txt.add_placeholder + "...")
+    st.session_state["todo_item"] = ""
+
+
+def main():
+    todo_list = functions.get_todo_list()
+
+    st.title(txt.app_title)
+
+    for todo_item in todo_list:
+        st.checkbox(todo_item)
+
+    st.text_input(
+        label="", placeholder=txt.add_placeholder + "...",
+        key="todo_item", on_change=add_todo_item
+    )
+
+    if "error" in st.session_state:
+        st.error(st.session_state["error"])
+        del st.session_state["error"]
+
+
+main()
